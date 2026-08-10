2b7fd4adbef129b4bb14555a87bd15700768574b
clauberlimaa-source <clauberlimaa@gmail.com>
2026-08-10 19:10:27 -0300

    Feat: documentacao.yml; remove erro serv 5500; validateDecimal.js, re organizado;

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-const express = require("express");
-
-const app = express();
+const app = require("./app");
 const port = 3000;
-
-// Endpoint para converter decimal para binário
-app.get("/to-binary/:decimal", (req, res) => {
- // Converte o parâmetro para número inteiro
- const decimal = parseInt(req.params.decimal, 10);
-
- // Se não for número decimal retorna um erro com status 400
- if (isNaN(decimal)) {
-   return res.status(400).json({ error: "Invalid decimal number" });
- }
-
- const binary = decimal.toString(2); // Converte o número decimal 
-//para binário 
-res.json({ decimal, binary }); // Retorna a resposta como JSON
-});
-
 app.listen(port, () => {
- console.log(`Servidor rodando na porta ${port}`);
+ console.log(`App running on http://localhost:${port}`);
 });
-
-
